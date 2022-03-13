fbcdbf1c88504d228793f8a28f8264e77a1c04f9
PhilB <phil.bridge@ixapps.co.uk>
2022-03-13 18:44:40 +0000

    Refactoring to extension methods

--- Play.Catalog.Service/Startup.cs
+++ Play.Catalog.Service/Startup.cs
@@ -30,31 +30,10 @@ namespace Play.Catalog.Service
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //clean up the mongo outputs 
-            BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
-            BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
-
             serviceSettings = Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
 
-            services.AddSingleton(serviceProvider => {
-                
-               var mongoDbSettings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-               var mongoClient = new MongoClient(mongoDbSettings.ConnectionString);
-               return mongoClient.GetDatabase(serviceSettings.ServiceName);
-
-            });
-
-            services.AddSingleton<IRepository<Item>>(serviceProvider =>
-            {
-                var database = serviceProvider.GetService<IMongoDatabase>();
-                return new MongoRepository<Item>(database, "items");
-            });
-
-            services.AddControllers(options =>{
-                options.SuppressAsyncSuffixInActionNames = false;
-            });
-
-
+            services.AddMongo()
+                .AddMongoRepository<Item>("items");
 
             services.AddSwaggerGen(c =>
             {
